bfee4a52460a4820b5bf8d8f98083b0fa72faf0c
Gimhan Fernando <gimhanfdo2004@gmail.com>
2025-06-03 16:11:37 +0530

    update padding in textField.dart

--- lib/screens/shared/textField.dart
+++ lib/screens/shared/textField.dart
@@ -15,7 +15,7 @@ class CustomTextField extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 25),
+      padding: const EdgeInsets.symmetric(horizontal: 100),
       child: TextField(
         controller: textController,
         obscureText: obscureText,
