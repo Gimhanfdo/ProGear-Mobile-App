9703e0425f931a90d01530de214645baec336af6
Gimhan Fernando <gimhanfdo2004@gmail.com>
2025-06-03 16:13:35 +0530

    updating padding in button.dart

--- lib/screens/shared/button.dart
+++ lib/screens/shared/button.dart
@@ -3,28 +3,29 @@ import 'package:flutter/material.dart';
 class CustomButton extends StatelessWidget {
   final Color buttonColor;
   final String buttonText;
-  final Function()? onTap;
 
-  const CustomButton({super.key, required this.buttonText, this.onTap, required this.buttonColor});
+  const CustomButton({super.key, required this.buttonText, required this.buttonColor});
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        padding: const EdgeInsets.all(25),
-        margin: const EdgeInsets.symmetric(horizontal: 25),
-        decoration: BoxDecoration(
-          color: buttonColor,
-          borderRadius: BorderRadius.circular(8),
-        ),
-        child: Center(
-          child: Text(
-            buttonText,
-            style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-              fontSize: 16,
+      onTap: (){},
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 100),
+        child: Container(
+          padding: const EdgeInsets.fromLTRB(25, 10, 25, 10),
+          decoration: BoxDecoration(
+            color: buttonColor,
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: Center(
+            child: Text(
+              buttonText,
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+              ),
             ),
           ),
         ),
