8e2077f62d9e9f9b01c1b6c620bd89b23cac83f3
Gimhan Fernando <gimhanfdo2004@gmail.com>
2025-06-07 10:50:10 +0530

    add navigationWrapper.dart

---
+++ lib/screens/navigationWrapper.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:progear_app/screens/cart.dart';
+import 'package:progear_app/screens/home.dart';
+import 'package:progear_app/screens/products.dart';
+import 'package:progear_app/screens/profile.dart';
+import 'package:progear_app/screens/shared/bottomNavBar.dart';
+
+class NavigationWrapper extends StatefulWidget {
+  const NavigationWrapper({super.key});
+
+  @override
+  State<NavigationWrapper> createState() => _NavigationWrapperState();
+}
+
+class _NavigationWrapperState extends State<NavigationWrapper> {
+  int currentIndex = 0;
+
+  List<Widget> pages = [
+    const Home(),
+    const ProductsPage(),
+    const CartPage(),
+    const ProfilePage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: pages[currentIndex],
+      bottomNavigationBar: BottomNavBar(
+        currentIndex: currentIndex,
+        onTap: (int index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
