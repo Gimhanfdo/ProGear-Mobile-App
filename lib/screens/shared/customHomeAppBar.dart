0ea5e93493e88464193fec2f97453e955cd6fa04
Gimhan Fernando <gimhanfdo2004@gmail.com>
2025-06-07 10:18:03 +0530

    create customHomeAppBar.dart

---
+++ lib/screens/shared/customHomeAppBar.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class CustomHomeAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const CustomHomeAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return AppBar(
+      backgroundColor: theme.colorScheme.surface,
+      title: Text(
+        'PROGEAR',
+        style: TextStyle(
+          fontFamily: 'Inter',
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      actions: [
+        IconButton(onPressed: (){}, icon: Icon(Icons.notifications))
+      ],
+    );
+  }
+  
+  @override
+  // TODO: implement preferredSize
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
