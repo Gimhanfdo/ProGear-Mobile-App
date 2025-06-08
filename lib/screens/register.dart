c29d16e4a47cd0d622acc3502801595994cf72af
Gimhan Fernando <gimhanfdo2004@gmail.com>
2025-06-08 12:26:14 +0530

    add validation method

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -11,10 +11,116 @@ class RegisterScreen extends StatelessWidget {
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
 
+  bool validated (BuildContext context){
+    final name = nameController.text;
+    final phoneNumber = phoneController.text;
+    final email = emailController.text;
+    final password = passwordController.text;
+
+    if (name.isEmpty || phoneNumber.isEmpty || email.isEmpty || password.isEmpty) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text("PROGEAR"),
+        content: Text("Empty fields founds. Please check and try again"),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: Text("OK"),
+          )
+        ],
+      ),
+    );
+    return false;
+  }
+
+  // Phone number validation
+  if (!RegExp(r'^\d{10}$').hasMatch(phoneNumber)) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text("PROGEAR"),
+        content: Text("Phone number should be 10 digits."),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: Text("OK"),
+          )
+        ],
+      ),
+    );
+    return false;
+  }
+
+  // Email validation
+  if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(email)) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text("PROGEAR"),
+        content: Text("Please enter a valid email address."),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: Text("OK"),
+          )
+        ],
+      ),
+    );
+    return false;
+  }
+
+  // Password validation
+  if (password.length < 6 || !RegExp(r'[A-Z]').hasMatch(password) || !RegExp(r'\d').hasMatch(password) || !RegExp(r'[!@#$%^&*(),.?":{}|<>]').hasMatch(password))  {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text("PROGEAR"),
+        content: Text("Password must be at least 6 characters long and consist of at least one uppercase, one digit and one special character."),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: Text("OK"),
+          )
+        ],
+      ),
+    );
+    return false;
+  }
+
+  else{
+    return true;
+  }
+  }
+
+  //Proceed to register if inputs are validated
+  void signUp(BuildContext context){
+    if (validated(context)){
+      showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder:
+            (context) => AlertDialog(
+              title: Text('PROGEAR'),
+              content: Text("Registrtion successful"),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    // Navigate back to Login screen
+                    Navigator.pop(context);
+                    Navigator.pop(context);
+                  },
+                  child: Text('OK'),
+                ),
+              ],
+            ),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[100],
       body: SafeArea( //To avoid notches and status bars
         child: SingleChildScrollView( //Scrollable view
           child: Center(
@@ -25,7 +131,7 @@ class RegisterScreen extends StatelessWidget {
                   'PROGEAR',
                   style: TextStyle(
                     fontFamily: 'Inter',
-                    fontSize: 24,
+                    fontSize: 28,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
@@ -35,7 +141,6 @@ class RegisterScreen extends StatelessWidget {
                 Text(
                   'Create your account now',
                   style: TextStyle(
-                    color: Colors.grey[800],
                     fontSize: 20,
                     fontWeight: FontWeight.bold,
                   ),
@@ -43,18 +148,8 @@ class RegisterScreen extends StatelessWidget {
           
                 SizedBox(height: 15),
           
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 100),
-                  child: Row(
-                    children: [
-                      Text('Full Name', style: TextStyle(color: Colors.grey[800])),
-                    ],
-                  ),
-                ),
-          
-                SizedBox(height: 5),
-          
                 CustomTextField(
+                  labelText: 'Full Name',
                   textController: nameController,
                   hintText: 'Enter your full name',
                   obscureText: false,
@@ -62,18 +157,8 @@ class RegisterScreen extends StatelessWidget {
           
                 SizedBox(height: 15),
           
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 100),
-                  child: Row(
-                    children: [
-                      Text('Phone number', style: TextStyle(color: Colors.grey[800])),
-                    ],
-                  ),
-                ),
-          
-                SizedBox(height: 5),
-          
                 CustomTextField(
+                  labelText: 'Phone Number',
                   textController: phoneController,
                   hintText: 'Enter your phone number',
                   obscureText: false,
@@ -81,64 +166,34 @@ class RegisterScreen extends StatelessWidget {
           
                 SizedBox(height: 15),
           
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 100),
-                  child: Row(
-                    children: [
-                      Text('Email', style: TextStyle(color: Colors.grey[800])),
-                    ],
-                  ),
-                ),
-          
-                SizedBox(height: 5),
-          
                 CustomTextField(
+                  labelText: 'Email Address',
                   textController: emailController,
                   hintText: 'Enter your email',
                   obscureText: false,
                 ),
           
                 SizedBox(height: 15),
-          
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 100),
-                  child: Row(
-                    children: [
-                      Text('Password', style: TextStyle(color: Colors.grey[800])),
-                    ],
-                  ),
-                ),
-          
-                SizedBox(height: 5),
+
                 CustomTextField(
+                  labelText: 'Password',
                   textController: passwordController,
                   hintText: 'Enter your password',
                   obscureText: true,
                 ),
           
-                SizedBox(height: 15),
-          
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 100),
-                  child: Row(
-                    children: [
-                      Text(
-                        'Forgot Password?',
-                        style: TextStyle(color: Colors.teal[600]),
-                      ),
-                    ],
-                  ),
-                ),
-          
-                const SizedBox(height: 25),
+                SizedBox(height: 25),
           
                 // Sign up button
                 CustomButton(
                   buttonText: 'Sign Up',
                   buttonColor: Colors.teal.shade600,
+                  onTap: (){
+                    signUp(context);
+                  },
                 ),
 
-                const SizedBox(height: 15),
+                const SizedBox(height: 25),
               ],
             ),
           ),
