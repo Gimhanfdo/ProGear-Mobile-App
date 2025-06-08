296821ed03e5729825261ed75feb309d4c7b98e5
Gimhan Fernando <gimhanfdo2004@gmail.com>
2025-06-08 06:36:13 +0530

    create cart page

--- lib/screens/cart.dart
+++ lib/screens/cart.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:progear_app/data/customerCart.dart';
+import 'package:progear_app/models/cartProduct.dart';
+import 'package:progear_app/screens/shared/cartProductCard.dart';
+import 'package:progear_app/screens/shared/customHomeAppBar.dart';
+import 'package:provider/provider.dart';
 
 class CartPage extends StatefulWidget {
   const CartPage({super.key});
@@ -10,6 +15,96 @@ class CartPage extends StatefulWidget {
 class _CartPageState extends State<CartPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Consumer<CustomerCart>(
+      builder: (context, cart, child) {
+        final List<CartProduct> cartItems = cart.getCart.values.toList();
+        final screenOrientation = MediaQuery.of(context).orientation;
+
+        return Scaffold(
+          appBar: screenOrientation == Orientation.landscape ? null : CustomHomeAppBar(), //Hide the app bar in the landscape view
+          body: Column(
+            children: [
+              Expanded(
+                child: ListView.builder(
+                  itemCount: cartItems.length,
+                  itemBuilder: (context, index) {
+                    return CartProductCard(cartProduct: cartItems[index]);
+                  },
+                ),
+              ),
+              if (cartItems.isNotEmpty)
+                Container(
+                  padding: const EdgeInsets.fromLTRB(16, 12, 16, 12),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.teal.shade100),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('Sub Total', style: TextStyle(fontSize: 18)),
+                          Text(
+                            'LKR ${cart.discountedTotal.toStringAsFixed(2)}',
+                            style: TextStyle(fontSize: 18),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 5),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('VAT (18%)', style: TextStyle(fontSize: 18)),
+                          Text(
+                            'LKR ${cart.vatValue.toStringAsFixed(2)}',
+                            style: TextStyle(fontSize: 18),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 5),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Total',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.teal[700],
+                            ),
+                          ),
+                          Text(
+                            'LKR ${cart.taxedTotal.toStringAsFixed(2)}',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.teal[700],
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 5),
+                      ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.teal.shade700,
+                        ),
+                        child: Text(
+                          'Proceed to Checkout',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+            ],
+          ),
+        );
+      },
+    );
   }
 }
