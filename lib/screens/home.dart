5ceeec6530d95c620c7f3aef8f402df4f9cb9369
Gimhan Fernando <gimhanfdo2004@gmail.com>
2025-06-08 06:38:47 +0530

    create home page

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,12 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:progear_app/screens/shared/bottomNavBar.dart';
+import 'package:progear_app/models/product.dart';
+import 'package:progear_app/data/allProducts.dart';
+import 'package:progear_app/screens/productDetails.dart';
+import 'package:progear_app/screens/products.dart';
+import 'package:progear_app/screens/profile.dart';
+import 'package:progear_app/screens/shared/customHomeAppBar.dart';
+import 'package:progear_app/screens/shared/productCard.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -9,18 +16,92 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  int currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomNavBar(
-        currentIndex: currentIndex,
-        onTap: (int index) {
-          setState(() {
-            currentIndex = index;
-          });
-        },
+      appBar: CustomHomeAppBar(),
+      body: 
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 150,
+                width: double.infinity,
+                child: CachedNetworkImage(
+                  //For image optimization
+                  imageUrl:
+                      'https://cms-static.asics.com/media-libraries/109692/file.jpg?1729645527521?20241023213849',
+                  fit: BoxFit.cover,
+                  placeholder:
+                      (context, url) =>
+                          const Center(child: CircularProgressIndicator()),
+                  errorWidget:
+                      (context, url, error) => const Icon(Icons.broken_image),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Row(
+                children: [
+                  Text(
+                    'Discounted Products',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(width: 5),
+                  Image.asset(
+                    'assets/icons/icons8-flash.png',
+                    width: 30,
+                    height: 30,
+                  ),
+                ],
+              ),
+              const SizedBox(height: 12),
+              SizedBox(
+                height:
+                    MediaQuery.of(context).size.height * 0.5, 
+                child: _displayDiscountedProducts(),
+              ),
+            ],
+          ),
+        ),
       ),
+    );
+  }
+
+  Widget _displayDiscountedProducts() {
+    final screenOrientation = MediaQuery.of(context).orientation;
+    final crossAxisCount = screenOrientation == Orientation.landscape ? 3 : 2;
+    final childAspectRatio =
+        screenOrientation == Orientation.landscape ? (100 / 110) : (100 / 140);
+
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: crossAxisCount,
+        childAspectRatio: childAspectRatio,
+        crossAxisSpacing: 12,
+        mainAxisSpacing: 12,
+      ),
+      scrollDirection: Axis.vertical,
+      itemCount: Allproducts.discountedProducts.length,
+      itemBuilder: (context, index) {
+        final discountedProduct = Allproducts.discountedProducts[index];
+        return GestureDetector(
+          child: ProductCard(product: discountedProduct),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder:
+                    (context) => ProductDetailsPage(
+                      product: Allproducts.discountedProducts[index],
+                    ),
+              ),
+            ); //Material Page Route
+          },
+        );
+      },
     );
   }
 }
