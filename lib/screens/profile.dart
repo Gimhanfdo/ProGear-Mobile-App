7b4b3c4e55ebc55ffbbff669eb742f840cfd31bd
Gimhan Fernando <gimhanfdo2004@gmail.com>
2025-06-08 20:15:52 +0530

    create profile.dart

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:progear_app/models/user.dart';
+import 'package:progear_app/screens/shared/button.dart';
+import 'package:progear_app/screens/shared/customHomeAppBar.dart';
 
 class ProfilePage extends StatefulWidget {
-  const ProfilePage({super.key});
+  ProfilePage({super.key});
 
   @override
   State<ProfilePage> createState() => _ProfilePageState();
@@ -10,6 +13,83 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final loggedInUser = LoggedInUser();
+
+    Widget profileRow(String title, String value) {
+      return Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            title,
+            style: TextStyle(
+              fontSize: 18,
+              color: Theme.of(context).colorScheme.onSecondaryContainer,
+            ),
+          ),
+          Text(
+            value,
+            style: TextStyle(
+              overflow: TextOverflow.ellipsis,
+              fontSize: 18,
+              color: Theme.of(context).colorScheme.onSecondaryContainer,
+            ),
+          ),
+        ],
+      );
+    }
+
+    return Scaffold(
+      appBar: const CustomHomeAppBar(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              SizedBox(
+                width: double.infinity,
+                child: Column(
+                  children: [
+                    const CircleAvatar(
+                      radius: 80,
+                      backgroundImage: AssetImage('assets/images/user.png'),
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text('Change Profile Picture'),
+                    ),
+                  ],
+                ),
+              ),
+
+              //Details
+              const SizedBox(height: 5),
+              const Divider(),
+              const SizedBox(height: 5),
+
+              Text(
+                'Profile Information',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+
+              const SizedBox(height: 20),
+
+              profileRow('Name', loggedInUser.name),
+              profileRow('Contact Number', loggedInUser.phoneNumber),
+              profileRow('Email', loggedInUser.email),
+
+              const SizedBox(height: 40),
+
+              CustomButton(
+                buttonText: 'Log out',
+                buttonColor: Colors.red.shade700,
+                onTap: () {
+                  LoggedInUser.logOut(context);
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
