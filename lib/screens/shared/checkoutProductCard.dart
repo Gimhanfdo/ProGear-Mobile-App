a09690d19fcb56649d8be65029d7aad4852d15cb
Gimhan Fernando <gimhanfdo2004@gmail.com>
2025-06-08 20:15:17 +0530

    create checkoutProductCard.dart

---
+++ lib/screens/shared/checkoutProductCard.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:progear_app/models/cartProduct.dart';
+import 'package:progear_app/models/product.dart';
+
+class CheckoutProductCard extends StatelessWidget {
+  final CartProduct cartProduct;
+  const CheckoutProductCard({super.key, required this.cartProduct});
+
+  bool hasDiscount(Product product) {
+    return product.discountPercentage != null;
+  }
+
+  double calculateDiscountedPrice(Product product) {
+    return product.price * (1 - product.discountPercentage! / 100);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final product = cartProduct.product;
+    final quantity = cartProduct.quantity;
+
+    final bool isDiscounted = hasDiscount(product);
+    final double unitPrice =
+        isDiscounted ? calculateDiscountedPrice(product) : product.price;
+
+    final double totalPrice = unitPrice * quantity;
+
+    return ListTile(
+      leading: CircleAvatar(
+        radius: 30,
+        backgroundImage: NetworkImage(product.productImage),
+      ),
+      title: Text(
+        product.productName,
+        style: TextStyle(fontWeight: FontWeight.bold),
+      ),
+      subtitle: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'LKR ${totalPrice.toStringAsFixed(2)}',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          Text('Unit Price: LKR ${unitPrice.toStringAsFixed(2)}'),
+          Text('Quantity: $quantity'),
+        ],
+      ),
+    );
+  }
+}
